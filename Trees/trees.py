8e621252fa441f2c87a17e905de96c9004e9760d
andrew-antoniuk <a.antoniuk.pn@ucu.edu.ua>
2026-05-02 19:24:00 +0300

    Add methods for BinaryTreeADT

--- Trees/trees.py
+++ Trees/trees.py
@@ -2,6 +2,8 @@
 Docstring for Trees.trees
 """
 
+from collections import deque
+
 class BinaryTreeNode:
 
     """
@@ -21,3 +23,37 @@ class BinaryTree:
 
     def __init__(self, root = None):
         self.root = root
+
+    def append(self, value):
+
+        """
+        Docstring for append
+        """
+
+        new = BinaryTreeNode(value)
+
+        if self.is_empty():
+            self.root = new
+            return
+
+        queue = deque([self.root])
+        while queue:
+            node = queue.popleft()
+
+            if node.left is None: # check left
+                node.left = new
+                return
+            queue.append(node.left)
+
+            if node.right is None: # check right
+                node.right = new
+                return
+            queue.append(node.right)
+
+    def is_empty(self) -> bool:
+
+        """
+        Docstring for is_empty
+        """
+
+        return self.root is None

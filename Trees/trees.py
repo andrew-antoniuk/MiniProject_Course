e39d6cf4edbeec6a1a3037ba1fd789e14ad6a6ce
andrew-antoniuk <a.antoniuk.pn@ucu.edu.ua>
2026-05-02 19:23:04 +0300

    Create BinaryTreeADT

---
+++ Trees/trees.py
@@ -0,0 +1,23 @@
+"""
+Docstring for Trees.trees
+"""
+
+class BinaryTreeNode:
+
+    """
+    Docstring for BinaryTreeNode
+    """
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+
+    """
+    Docstring for BinaryTree
+    """
+
+    def __init__(self, root = None):
+        self.root = root

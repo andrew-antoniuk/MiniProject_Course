edd420a755c0448a8777eec1bae38ae663ab8efb
andrew-antoniuk <a.antoniuk.pn@ucu.edu.ua>
2026-05-02 19:39:41 +0300

    Add invert to BinaryTreeADT, update methods, fix pylint

--- Trees/trees.py
+++ Trees/trees.py
@@ -15,6 +15,15 @@ class BinaryTreeNode:
         self.left = None
         self.right = None
 
+    def __eq__(self, other):
+        return self.value == other.value and self.left == other.left and self.right == other.right
+
+    def __repr__(self):
+        return f"BinaryTreeNode(value={self.value}, left={self.left}, right={self.right})"
+
+    def __str__(self):
+        return f"Node with value {self.value}, left-child {self.left} and right-child {self.right}"
+
 class BinaryTree:
 
     """
@@ -57,3 +66,38 @@ class BinaryTree:
         """
 
         return self.root is None
+
+    def is_leaf(self, node: BinaryTreeNode) -> bool:
+
+        """
+        Leaf status of the node
+        """
+
+        return node.left is None and node.right is None
+
+    def is_root(self, node: BinaryTreeNode) -> bool:
+
+        """
+        Root status of the node
+        """
+
+        return node is self.root
+
+    def invert(self):
+
+        """
+        Swap children of each subtree in the tree
+        """
+
+        if self.is_empty():
+            return
+
+        queue = deque([self.root])
+        while queue:
+            node = queue.popleft()
+            if node:
+                node.left, node.right = node.right, node.left
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
